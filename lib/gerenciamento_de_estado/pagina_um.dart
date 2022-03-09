5ea128623794cccc8df24920dccc20db4635f540
Matheus Vitor de Oliveira Cruz <61215783+Matheus-Tankian@users.noreply.github.com>
2022-03-08 23:09:20 -0300

    api inicio

--- lib/gerenciamento_de_estado/pagina_um.dart
+++ lib/gerenciamento_de_estado/pagina_um.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttlelvl1/widgets/custom_buttom_widget.dart';
+import 'package:http/http.dart' as http;
 
 class PaginaUm extends StatefulWidget {
   const PaginaUm({Key? key}) : super(key: key);
@@ -16,9 +18,22 @@ class _PaginaUmState extends State<PaginaUm> {
 
   randon() async {
     for (int i = 0; i < 10; i++) {
-      await Future.delayed(Duration(seconds: 1));
+      await Future.delayed(const Duration(seconds: 1));
 
       valorAleatorio.value = Random().nextInt(1000);
+    }
+  }
+
+  callAPI() async {
+    var client = http.Client();
+    try {
+      var response = await client.get(
+        Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+      );
+      var decodedResponse = jsonDecode(response.body);
+      print(decodedResponse);
+    } finally {
+      client.close();
     }
   }
 
@@ -41,6 +56,13 @@ class _PaginaUmState extends State<PaginaUm> {
             disable: false,
             onPressed: () => randon(),
             title: "Custom BTN",
+            titleSize: 18,
+          ),
+          const SizedBox(height: 10),
+          CustomButtonWidget(
+            disable: false,
+            onPressed: () => callAPI(),
+            title: "Api BTN",
             titleSize: 18,
           ),
         ],
